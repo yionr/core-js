8f98ea7ea776259bfd1278d525e6f55d6553f759
Denis Pushkarev <zloirock@zloirock.ru>
2023-07-02 11:32:48 +0700

    rename per https://github.com/ljharb/proposal-dataview-get-set-uint8c/commit/164b8e63f9e1ee71d24ea40affdb34b692f6a195

--- packages/core-js/full/data-view/index.js
+++ packages/core-js/full/data-view/index.js
@@ -1,5 +1,5 @@
 var parent = require('../../actual/data-view');
-require('../../modules/esnext.data-view.get-uint8c');
-require('../../modules/esnext.data-view.set-uint8c');
+require('../../modules/esnext.data-view.get-uint8-clamped');
+require('../../modules/esnext.data-view.set-uint8-clamped');
 
 module.exports = parent;
